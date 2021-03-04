2b33bd81dd62942439c9b20fad8bffd23d37f6fc
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-04 10:09:43 +0530

    login and logout

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,9 +11,12 @@ import { Avatar } from "@material-ui/core";
 import MicIcon from "@material-ui/icons/Mic";
 import HeadsetIcon from "@material-ui/icons/Headset";
 import SettingsIcon from "@material-ui/icons/Settings";
-
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
   return (
     <div className="Sidebar">
       <div className="sidebar__top">
@@ -49,10 +52,10 @@ function Sidebar() {
       </div>
 
       <div className="sidebar__profile">
-        <Avatar />
+        <Avatar src={user.photo} onClick = {() => auth.signOut()} />
         <div className="sidebar__profileinfo">
-          <h3>@rohan</h3>
-          <p>#thisIsMyId</p>
+          <h3>{user.displayName}</h3>
+          <p>{user.uid.substring(0, 5)}</p>
         </div>
         <div className="sidebar__profileIcons">
           <MicIcon />
